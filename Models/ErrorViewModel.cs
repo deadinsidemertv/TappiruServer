d95d3df0e1d5abd76e9a1d7ac1f564e16f593bea
zxcneroqwe <deadinsidemertv@mail.ru>
2026-04-04 12:20:59 +0300

    new

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TappiruServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
