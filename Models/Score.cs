3a52afb8c1f0e3be741acf7df776a8e95ff3ef1a
zxcneroqwe <deadinsidemertv@mail.ru>
2026-04-16 08:02:44 +0300

    Update Score.cs

--- Models/Score.cs
+++ Models/Score.cs
@@ -1,7 +1,12 @@
-﻿namespace TappiruServer.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TappiruServer.Models
 {
     public class Score
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string UserId { get; set; }
         public ApplicationUser User { get; set; }
